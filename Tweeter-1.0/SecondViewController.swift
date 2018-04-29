f88ff7f0bcefcd16975a7fe6c67c94ad0cce1757
Jasper Avtarovski <javtarovski@icloud.com>
2018-04-29 18:15:20 +1000

    Added Xcode Project

---
+++ Tweeter-1.0/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tweeter-1.0
+//
+//  Created by Jasper Avtarovski on 29/4/18.
+//  Copyright © 2018 Jasper Avtarovski. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
